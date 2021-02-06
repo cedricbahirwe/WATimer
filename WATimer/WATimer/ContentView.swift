4ea0ef2e5b7183b1b5489630956ef491d49dd733
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-02-07 00:26:02 +0200

    Update source paths

--- WATimer/WATimer/ContentView.swift
+++ WATimer/WATimer/ContentView.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+        @Environment(\.scenePhase) var scenePhase
     var body: some View {
-//        Home()
+        //        Home()
         ScrollView(showsIndicators: false) {
             TaskView()
         }
